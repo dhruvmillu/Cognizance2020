949d28966ce42582def35b43ec0b9ac6553f99c4
Dhruv Millu <dhruvmillu02@gmail.com>
2020-12-18 00:22:59 +0530

    Update name_shortner.java

--- task-4/name_shortner.java
+++ task-4/name_shortner.java
@@ -3,7 +3,7 @@ public class name_shortner {
     
     public static void main(String args[]){
         Scanner s = new Scanner(System.in);
-        System.out.println("enter the number of rows you want");
+        System.out.println("enter your name");
         String a = s.nextLine();
         int k,l=0;
         k= a.lastIndexOf(" ");
@@ -14,7 +14,6 @@ public class name_shortner {
                 res=res+a.charAt(l)+". ";
                 l=i+1;
             }
-            System.out.println(c);
         }
        res=res+a.substring(k+1);
         System.out.println(res);
